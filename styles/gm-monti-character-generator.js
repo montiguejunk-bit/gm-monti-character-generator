f23dcff2a9ce6864a0cdef4038e7132cb99e8ee7
montiguejunk-bit <montiguejunk@gmail.com>
2026-07-06 11:13:27 -0400

    Create first module files

---
+++ styles/gm-monti-character-generator.js
@@ -0,0 +1,6 @@
+Hooks.once("ready", () => {
+  console.log("====================================");
+  console.log("GM Monti Character Generator v0.0.1");
+  console.log("Module Loaded Successfully!");
+  console.log("====================================");
+});
